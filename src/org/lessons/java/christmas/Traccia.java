7b73edb33f372f058bd1f9f8a4c11154a43d5bd4
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-06-09 15:23:21 +0200

    finito es no bonus

--- src/org/lessons/java/christmas/Traccia.java
+++ src/org/lessons/java/christmas/Traccia.java
@@ -1,7 +1,5 @@
 //Package: org.lessons.java.christmas
 //
-//        Siamo a dicembre, è tempo di pensare alla lista dei regali di Natale e vogliamo creare un programma che memorizzi la nostra lista.
-//
 //        Il programma deve chiedere il nome del regalo da inserire in lista e salvarlo in un ArrayList (finchè l’utente non decide di fermarsi).
 //
 //        Ad ogni iterazione mostrare la lunghezza della lista e chiedere all’utente se vuole continuare aggiungendo un nuovo regalo.
--- src/org/lessons/java/christmas/WishList.java
+++ src/org/lessons/java/christmas/WishList.java
@@ -1,4 +1,44 @@
 package org.lessons.java.christmas;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class WishList {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> wishList = new ArrayList<>();
+        boolean basta = false;
+
+        do {
+            System.out.println("Inserisci di seguito la lista dei regali che vorresti ricevere! Aggiungine e scrivi 'basta' per fermarti");
+
+            String present = scanner.nextLine();
+            switch (present) {
+                case "basta":
+                    basta = true;
+                    break;
+                default:
+                    try {
+                        wishList.add(present);
+                        System.out.println("Hai scelto di aggiungere '" + present + "' alla tua wishlist");
+                    } catch (RuntimeException e) {
+                        System.out.println("Errore durante l'esecuzione");
+                    }
+                    break;
+            }
+
+            if (!present.equals("basta")) {
+                System.out.println("La tua WishList al momento contiene: " + wishList.size() + " Elemento/i");
+            }
+        } while (!basta);
+
+        System.out.println("Hai chiuso la tua lista con: " + wishList.size() + " Elemento/i");
+        Collections.sort(wishList);
+        System.out.println("La wishlist contiene: " + wishList);
+
+        scanner.close();
+    }
 }
